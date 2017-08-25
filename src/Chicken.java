380d17e654f78eab0c7963ef273d0e074411744a
munens <kaumbuthom@yahoo.com>
2017-08-24 19:59:11 -0700

    Sec. 1, lec. 8: Interfaces and abstractions: returned chicken class to correct state

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,11 +1,14 @@
 
-public class Chicken extends Bird {
+public class Chicken extends Bird implements Flyable{
 
 	public Chicken(String commonName, int noOfLegs, double averageHeight, int averageWeight, String color) {
 		super(commonName, noOfLegs, averageHeight, averageWeight, color);
 	}
-	
+
+	@Override
 	public void fly() {
-		System.out.println("I am a chicken and I cannot fly");
+		// TODO Auto-generated method stub
+		
 	}
+
 }
